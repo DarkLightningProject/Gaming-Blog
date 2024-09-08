8a4b702ed00619e96694f23536eae5c415909c06
DarkLightningProject <ripudamansinghmultani@gmail.com>
2024-09-08 13:10:13 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,8 @@
 echo "BUILD START"
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
+mkdir -p staticfiles_build
+
+# Move collected static files to the `staticfiles_build` folder
+mv static/* staticfiles_build/
 echo "BUILD END"
