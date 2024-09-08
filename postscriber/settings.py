93d71c47c7a3d91e3efd57b93a51f30802332201
DarkLightningProject <ripudamansinghmultani@gmail.com>
2024-09-08 13:30:20 +0530

    Update settings.py

--- postscriber/settings.py
+++ postscriber/settings.py
@@ -198,3 +198,6 @@ EMAIL_HOST_USER = 'email@gmail.com'
 EMAIL_HOST_PASSWORD = 'switzerland'  # App password if using 2FA
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
