47851edd66d9ea5d2c10c64e673d7671cf9a0566
DarkLightningProject <ripudamansinghmultani@gmail.com>
2024-09-08 13:50:26 +0530

    Update settings.py

--- postscriber/settings.py
+++ postscriber/settings.py
@@ -201,3 +201,18 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
+
+
